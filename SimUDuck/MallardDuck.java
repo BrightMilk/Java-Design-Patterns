de036999ca4519b2fd073dceb4baf32cdd84b4d7
Artem Medyanik <amedyanik.dev@gmail.com>
2020-11-08 01:25:43 +0300

    Added Empty classes for SimUDuck

---
+++ SimUDuck/MallardDuck.java
@@ -0,0 +1,5 @@
+package SimUDuck;
+
+public class MallardDuck {
+    
+}
---
+++ SimUDuck/Program.java
@@ -0,0 +1,7 @@
+package SimUDuck;
+
+public class Program {
+    public static void main(String[] args) {
+        /* Code */
+    }
+}
