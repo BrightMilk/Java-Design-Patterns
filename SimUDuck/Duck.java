69473017c752997738e328e5d7a4de6dd2ca077b
Artem Medyanik <amedyanik.dev@gmail.com>
2020-11-08 15:45:29 +0300

    Added abstract superclass Duck

--- SimUDuck/Duck.java
+++ SimUDuck/Duck.java
@@ -1,5 +1,24 @@
 package SimUDuck;
 
-public class Duck {
-    
+/**
+ * Define Duck abstract superclass.
+ * All ducks can quack and swim; superclass provides generic implementation code.
+ */
+abstract public class Duck {
+
+    public void swim() {
+        System.out.println("I can swim");
+    }
+
+    public void quack() {
+        System.out.println("Quack");
+    }
+
+    /**
+     * The display () method is declared abstract 
+     * because all subtypes are displayed differently.
+     * 
+     * The subtype of each particular kind implements its specific version of display ().
+     */
+    abstract public void display();
 }
